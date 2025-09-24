a6cb5f9a7a0a09af978903b14360bcd456e8cd4d
oleksandr <aleksandrprodan@ukr.net>
2025-09-24 10:56:26 +0300

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -9,15 +9,3 @@ newInput.addEventListener("input", (event) => {
     newSpan.textContent = trimName;
 }
 });
-
-newInput.addEventListener("blur", () => {
-  const trimmed = newInput.value.trim();
-
-  if (trimmed !== "") {
-    newSpan.textContent = trimmed;
-  } else {
-    newSpan.textContent = "Anonymous";
-  }
-
-  newInput.value = "";
-});
